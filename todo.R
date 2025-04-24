6f53cb7250e5e8fe0968b7a91ad7f29c02fbaa5c
Mason Rodrigue <dlf514@york.ac.uk>
2025-04-24 15:30:01 +0100

    add_task function created

---
+++ todo.R
@@ -0,0 +1,51 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages({
+  library(argparse)
+})
+
+TASK_FILE <- ".tasks.txt" # nolint
+
+add_task <- function(task) {
+  write(task_description,
+        file = TASK_FILE,
+        append = TRUE)
+}
+
+list_tasks <- function() {
+
+}
+
+remove_task <- function(index) {
+
+}
+
+main <- function(args) {
+
+  if (!is.null(args$add)) {
+    add_task(args$add)
+  } else if (args$list) {
+    tasks <- list_tasks()
+    print(tasks)
+  } else if (!is.null(args$remove)) {
+    remove_task(args$remove)
+  } else {
+    print("Use --help to get help on using this program")
+  }
+}
+
+
+if (sys.nframe() == 0) {
+
+  # main program, called via Rscript
+  parser <- ArgumentParser(description = "Command-line Todo List")
+  parser$add_argument("-a", "--add",
+                      help = "Add a new task")
+  parser$add_argument("-l", "--list",
+                      action = "store_true",
+                      help = "List all tasks")
+  parser$add_argument("-r", "--remove",
+                      help = "Remove a task by index")
+
+  args <- parser$parse_args()
+  main(args)
+}
